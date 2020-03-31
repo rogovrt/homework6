3be5af43a2a6156a1cbca020e2e27ea2bcfec2a6
rogovrt <49844050+rogovrt@users.noreply.github.com>
2020-03-31 13:33:10 +0300

    mail_parser with corrections

--- mail_parser.cpp
+++ mail_parser.cpp
@@ -4,14 +4,9 @@
 #include <algorithm>
 
 int main() {
-    std::string data =  ".apdaNvalid@gmail.com one more "  
-                        "lo.pp.pl@lol.su trash " //correct
-                        "hello@world.edu some text " //correct
-                        "tru|e@ma-il.ru JJ " //correct
-                        "ex@-unwork.org";
-    std::regex pattern (R"(\s[^.]([[:alnum:]!#$%&'*+-/=?^_`{|}~]+[.]?)*[^.]@([^-][[:alnum:]-]+.[[:alnum:]-]+[^-]))");
-    //локалка: любые буквы, цифры, символы; начинать и заканчивать не точкой, внутри не более одной к ряду
-    //домен: буквы, цифры, - (не в конце и не в начале)
+    std::string data =  R"(.apdaNvalid@gmail.com one more lo.pp.pl@lol.su trash hello@world.edu some text tru|e@ma-il.ru JJ ex@-unwork.org)";
+    //correct cout : lol.su world.edu ma-il.ru
+    std::regex pattern(R"(\s[^\.]([[:alnum:]!#$%&'*+-/=?^_`{|}~]+[\.]?)+[^\.]@([^-][[:alnum:]-]+(\.[[:alnum:]-]*)[^-]))");
     std::sregex_iterator begin(data.cbegin(), data.cend(), pattern);
     std::sregex_iterator end;
     std::for_each(begin, end, [](const std::smatch& m) {
