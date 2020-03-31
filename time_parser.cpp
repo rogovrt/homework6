173cfa627958a16779427cf7427328b5126b83e4
rogovrt <49844050+rogovrt@users.noreply.github.com>
2020-03-31 14:04:46 +0300

    Corrections in time_parser.cpp

--- time_parser.cpp
+++ time_parser.cpp
@@ -4,12 +4,9 @@
 #include <algorithm>
 
 int main() {
-    std::string data = "data 2003.4.6 "
-        "again 2099.12.31 "
-        "time 09:2:55 "
-        "again 12:32:45 ";
-    std::regex pattern_data(R"([12][09][0-9]{2}.1?[0-9].[123]?[0-9])");
-    std::regex pattern_time(R"([012]?[0-9]:[0-5]?[0-9]:[0-5][0-9])");
+    std::string data = R"(data 2003.04.06 again 1058.12.32 time 09:27:55 again 12:32:45 plus 25:14:40)";
+    std::regex pattern_data(R"(([0-2][0-9]{3})\.(0[1-9]|1[0-2])\.(0[1-9]|[1-2][0-9]|3[0-1]))");
+    std::regex pattern_time(R"(([0-1][0-9]|2[0-3])(:[0-5][0-9]){2})");
     std::sregex_iterator begin(data.cbegin(), data.cend(), pattern_data);
     std::sregex_iterator end;
     std::cout << "data\n";
@@ -21,6 +18,5 @@ int main() {
     std::for_each(begin0, end, [](const std::smatch& m) {
         std::cout << m.str(0) << std::endl;
         });
-    system("pause");
     return 0;
 }
